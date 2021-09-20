10be98448d15dfac0064960c51e0df98eda36029
Riya <riyaruchi139@gmail.com>
2021-09-20 14:55:41 +0530

    changes done

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 function CardItem(props) {
   return (
     <>
+
       <li className='cards__item'>
         <Link className='cards__item__link' to={props.path}>
           <figure className='cards__item__pic-wrap' data-category={props.label}>
@@ -21,5 +22,6 @@ function CardItem(props) {
     </>
   );
 }
+
 
 export default CardItem;
